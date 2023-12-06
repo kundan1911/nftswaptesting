6125a762864116cfdd20d56728ab8720eacaf731
kundan1911 <83800542+kundan1911@users.noreply.github.com>
2023-12-06 12:50:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,13 +4,14 @@ import { MNain } from './Pages/Main';
 import Multistep from './Pages/MultiStepForm';
 import Posts from './Pages/Posts';
 import DirectTrade from './Pages/DirectTrade';
+import Orders from './Pages/Orders'
 // import RoadMap from './Pages/RoadMap';
 import { useConnect, useAccount } from "wagmi";
 import { MetaMaskConnector } from "wagmi/connectors/metaMask";
 
 
 function App() {
-  const { address, isConnected } = useAccount();
+  const { address, isConnected } = useAccount()
   const { connect } = useConnect({
     connector: new MetaMaskConnector(),
   });
@@ -21,9 +22,10 @@ function App() {
       <Navbar connect={connect} isConnected={isConnected} address={address}  />
       <Routes>
         <Route exact path="/" element={<MNain/>} />
-        <Route exact path="/DirectTrade" element={<DirectTrade/>}/>
+        <Route exact path="/DirectTrade" element={<DirectTrade address={address}  />}/>
         <Route exact path="/create-post" element={<Multistep/>}/>
         <Route exact path="/Posts" element={<Posts />}/>
+        <Route exact path="/Orders" element={<Orders address={address} />}/>
       </Routes>
     </Router>
   );
